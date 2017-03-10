4918fcf7d72c7a3897467f1619a8d3435075285d
Ilia Vasilev <vasilya93@gmail.com>
2017-03-10 18:56:45 +0100

    Implemented border placement script
    Implemented basic version of the script, which allows to look
    through the background images in the directory, to select borders
    of the box in it, and to write those borders into configuration
    file.

---
+++ set_box_borders.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+import numpy as np
+import ConfigParser
+import io
+import cv2
+
+from os import listdir
+from os.path import isfile, join, splitext
+
+BACKGROUND_DIR = "sources/background"
+FILENAME_CONFIG = "config.ini"
+WINDOW_NAME = "Background"
+IMAGE_EXTENSIONS = [".jpg", ".png"]
+
+MODE_SET_TOP = 0
+MODE_SET_BOTTOM = 1
+MODE_SET_LEFT = 2
+MODE_SET_RIGHT = 3
+
+ProgramMode = MODE_SET_TOP
+ImageOriginal = np.zeros((0, 0, 3), np.uint8)
+ImageShown = np.zeros((0, 0, 3), np.uint8)
+
+TOP = -1
+BOTTOM = -1
+LEFT = -1
+RIGHT = -1
+
+
+def drawBorders():
+    global TOP, BOTTOM, LEFT, RIGHT
+    global ImageShown
+    ImageShown = ImageOriginal.copy()
+    height, width, num_channels = ImageShown.shape
+    lineWidth = 2
+    if TOP != -1:
+        cv2.line(ImageShown, (0, TOP), (width - 1, TOP), (255, 0, 0), lineWidth)
+        cv2.line(ImageShown, (0, TOP + lineWidth), (width - 1, TOP + lineWidth), (255, 255, 255), lineWidth)
+    if BOTTOM != -1:
+        cv2.line(ImageShown, (0, BOTTOM), (width - 1, BOTTOM), (0, 255, 0), lineWidth)
+        cv2.line(ImageShown, (0, BOTTOM - lineWidth), (width - 1, BOTTOM - lineWidth), (255, 255, 255), lineWidth)
+    if LEFT != -1:
+        cv2.line(ImageShown, (LEFT, 0), (LEFT, height - 1), (255, 0, 0), lineWidth)
+        cv2.line(ImageShown, (LEFT + lineWidth, 0), (LEFT + lineWidth, height - 1), (255, 255, 255), lineWidth)
+    if RIGHT != -1:
+        cv2.line(ImageShown, (RIGHT, 0), (RIGHT, height - 1), (0, 255, 0), lineWidth)
+        cv2.line(ImageShown, (RIGHT - lineWidth, 0), (RIGHT - lineWidth, height - 1), (255, 255, 255), lineWidth)
+
+def mouseHandler(event, x, y, flags, param):
+    global TOP, BOTTOM, LEFT, RIGHT
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if ProgramMode == MODE_SET_LEFT:
+            LEFT = x
+        elif ProgramMode == MODE_SET_RIGHT:
+            RIGHT = x
+        elif ProgramMode == MODE_SET_BOTTOM:
+            BOTTOM = y
+        elif ProgramMode == MODE_SET_TOP:
+            TOP = y
+        drawBorders()
+
+
+def resetBorders():
+    global TOP, BOTTOM, LEFT, RIGHT
+    TOP = -1
+    BOTTOM = -1
+    LEFT = -1
+    RIGHT = -1
+
+def changeProgramMode(keyCode):
+    global ProgramMode
+    doChangeImage = False
+    doExit = False
+    if keyCode == 27: # ESC
+        doExit = True 
+    elif keyCode == 32: # Space
+        doChangeImage = True 
+    elif keyCode == 97: # A
+        ProgramMode = MODE_SET_LEFT
+    elif keyCode == 100: # D
+        ProgramMode = MODE_SET_RIGHT
+    elif keyCode == 115: # S
+        ProgramMode = MODE_SET_BOTTOM
+    elif keyCode == 119: # W
+        ProgramMode = MODE_SET_TOP
+    return (doChangeImage, doExit)
+
+def isImageExtension(checkedExtension):
+    for extension in IMAGE_EXTENSIONS:
+        if extension == checkedExtension:
+            return True
+    return False
+
+def getImageNames():
+    filenames = [f for f in listdir(BACKGROUND_DIR) if isfile(join(BACKGROUND_DIR, f))]
+    image_names = []
+    for full_name in filenames:
+        filename, file_extension = splitext(full_name)
+        if isImageExtension(file_extension):
+            image_names.append(full_name)
+    return image_names
+
+def writeConfigFile(fileName, dictionary):
+    configFile = open(fileName, 'w')
+    config = ConfigParser.ConfigParser()
+    for key in dictionary:
+        config.add_section(key)
+        for subKey in dictionary[key]:
+            config.set(key, subKey, dictionary[key][subKey])
+    config.write(configFile)
+    configFile.close()
+
+def addImageData(imageData, imageName):
+    if TOP != -1 and BOTTOM != -1 and LEFT != -1 and RIGHT != -1:
+        imageData[imageName] = {}
+        imageData[imageName]["top"] = TOP
+        imageData[imageName]["bottom"] = BOTTOM
+        imageData[imageName]["left"] = LEFT
+        imageData[imageName]["right"] = RIGHT
+
+imageData = {}
+imageNames = getImageNames()
+cv2.namedWindow(WINDOW_NAME)
+cv2.setMouseCallback(WINDOW_NAME, mouseHandler)
+for imageName in imageNames:
+    resetBorders()
+    image_path = BACKGROUND_DIR + "/" + imageName
+    ImageOriginal = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    ImageShown = ImageOriginal.copy()
+    doChangeImage = False; doExit = False
+    while not (doChangeImage or doExit):
+        cv2.imshow(WINDOW_NAME, ImageShown)
+        keyCode = cv2.waitKey(20)
+        doChangeImage, doExit = changeProgramMode(keyCode)
+    addImageData(imageData, imageName)
+    if doExit:
+        break
+if len(imageData) != 0:
+    pathConfig = BACKGROUND_DIR + "/" + FILENAME_CONFIG
+    writeConfigFile(pathConfig, imageData)
