4eb3629a6e728f954d6ea62b63ddcdc8c7bce5b5
Ilia Vasilev <vasilya93@gmail.com>
2017-04-20 14:32:17 +0200

    Rectangle: differentiated intersect and overlap
    In object placement we can often allow objects opvelap, but it is
    not desirable to allow them intersect, as hidden area of one
    is too big in this case. Thus, I made it possible to check that
    objects just overlap, but do not intersect.

--- rectangle.py
+++ rectangle.py
@@ -7,34 +7,45 @@ class Rectangle:
         self.left = left
         self.right = right
 
-    def doesIntersectRectangles(self, rectangles):
-        doesInt = False
+    def doesOverlapRectangles(self, rectangles):
+        doesOverlap = False
         for rect in rectangles:
             # top left corner
-            doesInt = doesInt or (self.left > rect.left and \
+            doesOverlap = doesOverlap or (self.left > rect.left and \
                 self.left < rect.right and self.top > rect.top and self.top < rect.bottom)
             # bottom left corner
-            doesInt = doesInt or (self.left > rect.left and \
+            doesOverlap = doesOverlap or (self.left > rect.left and \
                 self.left < rect.right and self.bottom > rect.top and self.bottom < rect.bottom)
             # top right corner
-            doesInt = doesInt or (self.right > rect.left and \
+            doesOverlap = doesOverlap or (self.right > rect.left and \
                 self.right < rect.right and self.top > rect.top and self.top < rect.bottom)
             # bottom right corner
-            doesInt = doesInt or (self.right > rect.left and \
+            doesOverlap = doesOverlap or (self.right > rect.left and \
                 self.right < rect.right and self.bottom > rect.top and self.bottom < rect.bottom)
 
-            doesInt = doesInt or (rect.left > self.left and \
+            doesOverlap = doesOverlap or (rect.left > self.left and \
                 rect.left < self.right and rect.top > self.top and rect.top < self.bottom)
-            doesInt = doesInt or (rect.left > self.left and \
+            doesOverlap = doesOverlap or (rect.left > self.left and \
                 rect.left < self.right and rect.bottom > self.top and rect.bottom < self.bottom)
-            doesInt = doesInt or (rect.right > self.left and \
+            doesOverlap = doesOverlap or (rect.right > self.left and \
                 rect.right < self.right and rect.top > self.top and rect.top < self.bottom)
-            doesInt = doesInt or (rect.right > self.left and 
+            doesOverlap = doesOverlap or (rect.right > self.left and 
                 rect.right < self.right and rect.bottom > self.top and rect.bottom < self.bottom)
 
-            doesInt = doesInt or (self.left < rect.left and \
+            if doesOverlap:
+                break
+
+        return doesOverlap
+
+    def doesIntersectRectangles(self, rectangles):
+        doesIntersect = False
+        for rect in rectangles:
+            doesIntersect = doesIntersect or (self.left < rect.left and \
                 self.right > rect.right and self.top > rect.top and self.bottom < rect.bottom)
-            doesInt = doesInt or (rect.left < self.left and \
+            doesIntersect = doesIntersect or (rect.left < self.left and \
                 rect.right > self.right and rect.top > self.top and rect.bottom < self.bottom)
 
-        return doesInt
+            if doesIntersect:
+                break
+
+        return doesIntersect
