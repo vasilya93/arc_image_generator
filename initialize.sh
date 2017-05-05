5914e7245a1fb54edf2a660e9eaae1ad503305f6
Ilia Vasilev <vasilya93@gmail.com>
2017-05-05 14:29:42 +0200

    Initialize: corrected bug in downloading images

--- initialize.sh
+++ initialize.sh
@@ -13,9 +13,9 @@ mkdir $OBJECTS_DIR
 mkdir $BG_DIR
 
 cd $OBJECTS_DIR
-wget --no-check-certificate "https://www.dropbox.com/s/gciezq481ive4jw/expo_eraser.png?dl=0"
-wget --no-check-certificate "https://www.dropbox.com/s/dsoa55g5vayxxy8/hand_weight.png?dl=0"
-wget --no-check-certificate "https://www.dropbox.com/s/rs8e6x95gpkillt/speed_stick.png?dl=0"
+wget --no-check-certificate "https://www.dropbox.com/s/gciezq481ive4jw/expo_eraser.png"
+wget --no-check-certificate "https://www.dropbox.com/s/dsoa55g5vayxxy8/hand_weight.png"
+wget --no-check-certificate "https://www.dropbox.com/s/rs8e6x95gpkillt/speed_stick.png"
 
 cd "../$BG_DIR"
-wget --no-check-certificate "https://www.dropbox.com/s/hc45y15t327wupy/box_white.jpg?dl=0"
+wget --no-check-certificate "https://www.dropbox.com/s/hc45y15t327wupy/box_white.jpg"
