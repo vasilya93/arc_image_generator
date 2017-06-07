b6a441c91cfed4411804d9e2c0468cdd2f99edf9
Ilia Vasilev <vasilya93@gmail.com>
2017-06-07 14:08:52 +0200

    Initialize: creating object separate directories
    Corrected initializing script so that it creates separate
    directories for every of the objects, as image generation script
    now needs that kind of arrangement of images.

--- initialize.sh
+++ initialize.sh
@@ -2,6 +2,9 @@
 
 SOURCES_DIR=sources
 OBJECTS_DIR=objects
+EXPO_ERASER_DIR=expo_eraser
+HAND_WEIGHT_DIR=hand_weight
+SPEED_STICK_DIR=speed_stick
 BG_DIR=background
 OUT_DIR=output
 
@@ -13,9 +16,20 @@ mkdir $OBJECTS_DIR
 mkdir $BG_DIR
 
 cd $OBJECTS_DIR
+mkdir $EXPO_ERASER_DIR
+cd $EXPO_ERASER_DIR
 wget --no-check-certificate "https://www.dropbox.com/s/gciezq481ive4jw/expo_eraser.png"
+cd ..
+
+mkdir $HAND_WEIGHT_DIR
+cd $HAND_WEIGHT_DIR
 wget --no-check-certificate "https://www.dropbox.com/s/dsoa55g5vayxxy8/hand_weight.png"
+cd ..
+
+mkdir $SPEED_STICK_DIR
+cd $SPEED_STICK_DIR
 wget --no-check-certificate "https://www.dropbox.com/s/rs8e6x95gpkillt/speed_stick.png"
+cd ..
 
 cd "../$BG_DIR"
 wget --no-check-certificate "https://www.dropbox.com/s/hc45y15t327wupy/box_white.jpg"
